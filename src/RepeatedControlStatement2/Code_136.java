2c93bea4e10b6461536da5313d13c63f9d5591d3
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-11-28 15:56:15 +0900

    [RepeatedControlStatement2 Code137]

--- src/RepeatedControlStatement2/Code_136.java
+++ src/RepeatedControlStatement2/Code_136.java
@@ -7,10 +7,15 @@ public class Code_136 {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
 		
-		for(int i = 1; i <= 10; i++) {
-			System.out.printf("%d ", num*i);
+		for(int x = 1; x <= num1; x++) {
+			for(int y = 1; y <= num2; y++) {
+				System.out.printf("%d ", x*y);
+			}
+			System.out.println();
 		}
+		
 	}
 }
