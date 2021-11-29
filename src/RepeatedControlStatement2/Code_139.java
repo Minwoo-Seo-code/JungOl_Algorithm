6a43fb7c62f256720bc43bb75ae0684bb4820c88
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-11-29 23:39:53 +0900

    [RepeatedControlStatement2 Code_139]

---
+++ src/RepeatedControlStatement2/Code_139.java
@@ -0,0 +1,29 @@
+package RepeatedControlStatement2;
+
+import java.util.Scanner;
+
+public class Code_139 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+
+		if(num1>num2){
+	        for(int i=1; i<=9; i++){
+	            for(int j=num1; j>=num2; j--){
+	            	System.out.printf("%d * %d = %2d   ", j, i, j*i);
+	            }
+	            System.out.println();
+	        }
+		} else {
+			for(int i=1; i<=9; i++){
+	            for(int j=num1; j<=num2; j++){
+	            	System.out.printf("%d * %d = %2d   ", j, i, j*i);
+	            }
+	            System.out.println();
+	        }
+		}
+	}
+}
