858f5faab90215458b9270da2548e45ce40468c8
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-11-29 19:05:01 +0900

    [RepeatedControlStatement2 Code_138]

---
+++ src/RepeatedControlStatement2/Code_137.java
@@ -0,0 +1,21 @@
+package RepeatedControlStatement2;
+
+import java.util.Scanner;
+
+public class Code_137 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		
+		for(int x = 1; x <= num1; x++) {
+			for(int y = 1; y <= num2; y++) {
+				System.out.printf("%d ", x*y);
+			}
+			System.out.println();
+		}
+		
+	}
+}
--- src/RepeatedControlStatement2/Code_138.java
+++ src/RepeatedControlStatement2/Code_138.java
@@ -2,15 +2,18 @@ package RepeatedControlStatement2;
 
 import java.util.Scanner;
 
-public class Code_136 {
+public class Code_138 {
 
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
 		int num = sc.nextInt();
-
-		for(int i = 1; i <= 10; i++) {
-			System.out.printf("%d ", num*i);
+		
+		for (int i = 1; i <= num; i++) {
+			for(int y = 1; y <= num; y++) {
+				System.out.printf("(%d, %d) ", i, y);
+			}
+			System.out.println();
 		}
 	}
 }
