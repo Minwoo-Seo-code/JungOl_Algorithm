0267f18b55f51df7ead5ef5f0fc9a1bbbe2db2ac
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-11-29 23:42:31 +0900

    [Star 10 X 10]

---
+++ src/Star/Star1.java
@@ -0,0 +1,14 @@
+package Star;
+
+public class Star1 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 1; i<=10; i++) {
+			for(int y = 1; y<=10; y++) {
+				System.out.printf("%c", '☆');
+			}
+			System.out.println();
+		}
+	}
+}
