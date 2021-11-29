2089629deb62951c8155785e8029767dafd26ba2
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-11-29 23:48:20 +0900

    [Star3 reserve star 10 to 1]

---
+++ src/Star/Star3.java
@@ -0,0 +1,14 @@
+package Star;
+
+public class Star3 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			for(int y = i; y < 10; y++) {
+				System.out.printf("%c", '☆');
+			}
+			System.out.println();
+		}
+	}
+}
