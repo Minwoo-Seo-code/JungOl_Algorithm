5aa412ad4a5da44ee8d2589fcdea45c052c170b7
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-11-30 23:35:11 +0900

    [Star Pattern3]

---
+++ src/Star/Star5.java
@@ -0,0 +1,21 @@
+package Star;
+
+public class Star5 {
+
+	public static void main(String[] args) {
+
+		int i, j;
+		int n = 10;
+		
+		for(i = 0; i < n; i++) {
+			for(j = 0; j < n; j++) {
+				if(i + j <= n-1) {//9, 
+					System.out.printf("%c", '*');
+				}else {
+					System.out.printf("%c", ' ');
+				}
+			}
+			System.out.println();
+		}
+	}
+}
