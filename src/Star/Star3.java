989349c8655d0312b511ffed70278ef15f6adebe
Minwoo-Seo-code <yangmeog1216@naver.com>
2021-11-30 23:27:09 +0900

    star pattern2

--- src/Star/Star3.java
+++ src/Star/Star3.java
@@ -3,7 +3,7 @@ package Star;
 public class Star3 {
 
 	public static void main(String[] args) {
-		
+
 		for(int i = 0; i < 10; i++) {
 			for(int y = i; y < 10; y++) {
 				System.out.printf("%c", '☆');
